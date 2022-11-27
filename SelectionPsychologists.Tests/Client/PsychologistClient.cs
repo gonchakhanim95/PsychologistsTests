ccb9bc01f9ed625e39c627e7d5ec13f07bcf33c6
gonchakhanim95 <goncha.kanim@gmail.com>
2022-11-27 21:05:59 +0400

    create/auth psy ant get list psys

--- SelectionPsychologists.Tests/Client/PsychologistClient.cs
+++ SelectionPsychologists.Tests/Client/PsychologistClient.cs
@@ -1,6 +1,11 @@
 ﻿using SelectionPsychologists.Tests.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using System.Text.Json;
 
 namespace SelectionPsychologists.Tests.Client
