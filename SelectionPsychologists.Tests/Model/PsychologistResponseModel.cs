84186789426633ab1bdf6d9351d0cf021ae5e86a
gonchakhanim95 <goncha.kanim@gmail.com>
2022-12-02 02:03:54 +0400

    Change Personal Data (Added Education)

---
+++ SelectionPsychologists.Tests/Model/PsychologistResponseModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SelectionPsychologists.Tests.Model
+{
+    public class PsychologistResponseModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("gender")]
+        public int Gender { get; set; }
+
+        [JsonPropertyName("workExperience")]
+        public int WorkExperience { get; set; }
+
+        [JsonPropertyName("education")]
+        public object Education { get; set; }
+
+        [JsonPropertyName("therapyMethods")]
+        public List<object> TherapyMethods { get; set; }
+
+        [JsonPropertyName("problems")]
+        public List<object> Problems { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+
+        /*public override bool Equals(object? obj)
+        {
+            return obj is PsychologistResponseModel model &&
+                   Name == model.Name &&
+                   LastName == model.LastName &&
+                   Gender == model.Gender &&
+                   WorkExperience == model.WorkExperience &&
+                   Price == model.Price;
+        }*/
+    }
+}
