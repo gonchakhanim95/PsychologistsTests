661dcea279a763e4759ad6378e654b8b09886bae
zzakirrQa <gadjievz777@gmail.com>
2022-12-01 18:49:31 +0400

    registration and autorization client

---
+++ SelectionPsychologists.Tests/Models/CheckClientModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SelectionPsychologists.Tests.Models
+{
+    public class CheckClientModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonPropertyName("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CheckClientModel model &&
+                   Name == model.Name &&
+                   LastName == model.LastName &&
+                   Email == model.Email &&
+                   PhoneNumber == model.PhoneNumber &&
+                   BirthDate == model.BirthDate;
+        }
+
+    }
+}
--- SelectionPsychologists.Tests/UnitTest1.cs
+++ SelectionPsychologists.Tests/UnitTest1.cs
@@ -1,16 +1,77 @@
+global using NUnit.Framework;
+using Dapper;
+using SelectionPsychologists.Tests.Client;
+using SelectionPsychologists.Tests.Models;
+using System.Data;
+using System.Data.SqlClient;
+
 namespace SelectionPsychologists.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+        private const string EMAIL = "guseyn123qwer23@gmail.com";
+        private const string PASSWORD = "Gus1Client";
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
+            ClientClient client = new ClientClient();
+
+            ClientRequestModel clientRegistrationModel = new ClientRequestModel()
+            {
+                Name = "guseyn",
+                LastName = "gusikov",
+                Password = PASSWORD,
+                Email = EMAIL,
+                PhoneNumber = "+71111111111",
+                BirthDate = new DateTime(2002,11,11)
+            };
+
+            int id = client.RegistrationClient(clientRegistrationModel);
+
+            //2oy test
+
+            AuthClientRequestModel clientAuthModel = new AuthClientRequestModel()
+            {
+                Email = EMAIL,
+                Password = PASSWORD
+            };
+
+            string token = client.AuthClient(clientAuthModel);
+
+            CheckClientModel expextedClient = new CheckClientModel()
+            {
+                Id =id,
+                Name = clientRegistrationModel.Name,
+                LastName = clientRegistrationModel.LastName,
+                Password = clientRegistrationModel.Password,
+                Email = clientRegistrationModel.Email,
+                PhoneNumber = clientRegistrationModel.PhoneNumber,
+                BirthDate = clientRegistrationModel.BirthDate
+            };
+
+            CheckClientModel actualClient = client.CheckClientById(id, token);
+
+
+
+            Assert.AreEqual(expextedClient, actualClient);
+
         }
+
+
+        [TearDown]
+        public void TD()
+        {
+            string connectionString = @"Data Source = 80.78.240.16; Initial Catalog = BBSK_PsychoDb4; Persist Security Info = True; User ID = student; Password = qwe!23;";
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+            dbConnection.Query($"delete from Ñlient where Email = '{EMAIL}'");
+            dbConnection.Close();
+        }
+        [OneTimeTearDown]
+        public void OTTD()
+        {
+
+         }
     }
 }
