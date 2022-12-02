1769dc824461bd4922aa6b9245e72a8901f4cc3b
gonchakhanim95 <goncha.kanim@gmail.com>
2022-12-02 22:47:02 +0400

    I changed few details

--- SelectionPsychologists.Tests/Model/PsychologistResponseModel.cs
+++ SelectionPsychologists.Tests/Model/PsychologistResponseModel.cs
@@ -28,7 +28,7 @@ namespace SelectionPsychologists.Tests.Model
         [JsonPropertyName("price")]
         public double Price { get; set; }
 
-        public override bool Equals(object? obj)
+        /*public override bool Equals(object? obj)
         {
             return obj is PsychologistResponseModel model &&
                    Name == model.Name &&
@@ -41,6 +41,6 @@ namespace SelectionPsychologists.Tests.Model
         public override string ToString()
         {
             return Name;
-        }
+        }*/
     }
 }
