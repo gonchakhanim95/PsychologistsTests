ec2c809668e282b9745ace0e7c0a2a82f0266a11
zzakirrQa <gadjievz777@gmail.com>
2022-12-03 13:57:50 +0400

    Получение всех психологов как клиент НАЧАЛО

--- SelectionPsychologists.Tests/UnitTest1.cs
+++ SelectionPsychologists.Tests/UnitTest1.cs
@@ -1,16 +1,36 @@
+global using NUnit.Framework;
 namespace SelectionPsychologists.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+
+        [OneTimeSetUp]
+        public void otsu()
         {
+
+        }
+        [SetUp]
+        public void su()
+        {
+
         }
 
         [Test]
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [TearDown]
+        public void td()
+        {
+
+        }
+        [OneTimeTearDown]
+        public void ottd()
+        {
+
         }
     }
 }
